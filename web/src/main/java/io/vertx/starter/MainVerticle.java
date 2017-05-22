0542889bc22b675c4192c1a69f11d812de5e6f9b
Boris Musatov <boris.musatov@gmail.com>
2017-05-22 21:21:32 +0200

    Fix resource name

--- web/src/main/java/io/vertx/starter/MainVerticle.java
+++ web/src/main/java/io/vertx/starter/MainVerticle.java
@@ -5,54 +5,30 @@ import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.handler.sockjs.BridgeOptions;
+import io.vertx.ext.web.handler.sockjs.PermittedOptions;
+import io.vertx.ext.web.handler.sockjs.SockJSHandler;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
-  public void start() {
+  public void start() throws Exception {
+    System.out.println("Starting...");
+
     Router router = Router.router(vertx);
-    router.route().handler(BodyHandler.create());
-    router.get("/person").handler(req -> {
-      String boris = Json.encode(new Person("Boris", 35));
-      req.response().end(boris);
-    });
-    router.post("/person").handler(req -> {
-      JsonObject json = req.getBodyAsJson();
-      vertx.eventBus().publish("topic-1", json);
-      req.response().end("Success");
-    });
-    router.get("/home").handler(req -> req.response().end("Home page"));
 
+    BridgeOptions options = new BridgeOptions().addOutboundPermitted(new PermittedOptions().setAddress("address"));
+    router.route("/eventbus/*").handler(SockJSHandler.create(vertx).bridge(options));
+
+    // Serve the static resources
+    router.route().handler(StaticHandler.create());
     vertx.createHttpServer().requestHandler(router::accept).listen(8080);
-  }
 
-  static class Person {
-    private String name;
-    private int age;
-
-    public String getName() {
-      return name;
-    }
-
-    public void setName(String name) {
-      this.name = name;
-    }
-
-    public int getAge() {
-      return age;
-    }
-
-    public void setAge(int age) {
-      this.age = age;
-    }
-
-    public Person() {
-    }
-
-    public Person(String name, int age) {
-      this.name = name;
-      this.age = age;
-    }
+    vertx.setPeriodic(5000, t -> {
+        vertx.eventBus().publish("address", "news from the web verticle!");
+      }
+    );
   }
 
 }
