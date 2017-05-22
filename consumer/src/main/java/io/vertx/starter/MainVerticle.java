e878efa177f9e2bb9c985a28e1936c626835fa64
Boris Musatov <boris.musatov@gmail.com>
2017-05-22 17:37:25 +0200

    Add consumer

--- consumer/src/main/java/io/vertx/starter/MainVerticle.java
+++ consumer/src/main/java/io/vertx/starter/MainVerticle.java
@@ -3,56 +3,25 @@ package io.vertx.starter;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
 
 public class MainVerticle extends AbstractVerticle {
 
+  private static final String STEPAN = "Stepan";
+  private static final Logger LOG = LoggerFactory.getLogger(MainVerticle.class);
+
   @Override
   public void start() {
-    Router router = Router.router(vertx);
-    router.route().handler(BodyHandler.create());
-    router.get("/person").handler(req -> {
-      String boris = Json.encode(new Person("Boris", 35));
-      req.response().end(boris);
+    String address = config().getString("address");
+    LOG.info(STEPAN + " is ready to get messages on the address: " + address);
+    vertx.eventBus().consumer(address, message -> {
+      JsonObject messageBody = (JsonObject)message.body();
+      LOG.info(STEPAN + " received message: " + messageBody);
+      message.reply("Thanks for the message!");
+      LOG.info(STEPAN + " sent reply");
     });
-    router.post("/person").handler(req -> {
-      JsonObject json = req.getBodyAsJson();
-      vertx.eventBus().publish("topic-1", json);
-      req.response().end("Success");
-    });
-    router.get("/home").handler(req -> req.response().end("Home page"));
-
-    vertx.createHttpServer().requestHandler(router::accept).listen(8080);
   }
-
-  static class Person {
-    private String name;
-    private int age;
-
-    public String getName() {
-      return name;
-    }
-
-    public void setName(String name) {
-      this.name = name;
-    }
-
-    public int getAge() {
-      return age;
-    }
-
-    public void setAge(int age) {
-      this.age = age;
-    }
-
-    public Person() {
-    }
-
-    public Person(String name, int age) {
-      this.name = name;
-      this.age = age;
-    }
-  }
-
 }
